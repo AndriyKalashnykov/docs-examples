1a5cf7c015afbb164e335210671e72aaeda1d90e
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2023-06-06 22:54:32 -0400

    fix: updated client
    Signed-off-by: Andriy Kalashnykov <AndriyKalashnykov@gmail.com>

--- apollo-client/v3/getting-started/src/App.js
+++ apollo-client/v3/getting-started/src/App.js
@@ -1,28 +1,26 @@
 import { useQuery, gql } from '@apollo/client';
 
-const GET_LOCATIONS = gql`
-  query getLocations {
-    locations {
-      id
-      name
-      description
-      photo
+const GET_BOOKS = gql`
+  query getBooks {
+    books {
+      title
+      author
     }
   }
 `;
 
-function DisplayLocations() {
-  const { loading, error, data } = useQuery(GET_LOCATIONS);
+function DisplayBooks() {
+  const { loading, error, data } = useQuery(GET_BOOKS);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
-  return data.locations.map(({ id, name, description, photo }) => (
+  return data.books.map(({ id, title, author }) => (
     <div key={id}>
-      <h3>{name}</h3>
-      <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+      <b>Title:</b>
+      <h3>{title}</h3>
       <br />
-      <b>About this location:</b>
-      <p>{description}</p>
+      <b>Author:</b>
+      <p>{author}</p>
       <br />
     </div>
   ));
@@ -38,7 +36,7 @@ export default function App() {
         </span>
       </h2>
       <br />
-      <DisplayLocations />
+      <DisplayBooks />
     </div>
   );
 }
